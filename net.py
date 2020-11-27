4d64f4b6776c40887b0dc26f82252d4233e5a4b7
Evgeny Shalnov <shalnov.eugen@gmail.com>
2020-11-27 12:34:07 +0300

    Significantly increase forward pass for raw event stream

--- net.py
+++ net.py
@@ -1,6 +1,11 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
+try:
+    from torch_scatter import scatter_max, scatter_sum
+except:
+    scatter_max = None
+    scatter_sum = None
 
 import numpy as np
 
@@ -71,50 +76,54 @@ class UpsampleBlock(nn.Module):
                 mode='reflect')
         return self.conv(x)
 
-def compute_event_image(events, start, stop, imsize):
-    ''' computes event image. Unfortunately on CPU, because of absense of torch.index_max_ function
+def compute_event_image(events, start, stop, imsize, device='cpu', dtype=torch.float32):
+    ''' computes event image
     '''
+    assert scatter_max is not None, f'follow https://github.com/rusty1s/pytorch_scatter#installation to install torch_scatter'
     bs = len(start)
-    if isinstance(events, torch.Tensor):
-        events = events.detach().cpu().numpy()
-        start = start.detach().cpu().numpy()
-        stop = stop.detach().cpu().numpy()
+    if not isinstance(events, torch.Tensor):
+        events = torch.tensor(events, device=device)
+        start = torch.tensor(start, device=device, dtype=dtype)
 
-    x = events[:, 0].astype(int)
-    y = events[:, 1].astype(int)
-    t = events[:, 2]
-    p = events[:, 3].astype(int)
-    b = events[:, 4].astype(int)
-
-    # index of the last event for each timestamp
-    uniq_b, num_events = np.unique(b, return_counts=True)
-    if uniq_b.size < bs:
-        # seems, there is an empty set of events along samples
-        pass
-    shift = np.zeros(bs, dtype=int)
-    shift[uniq_b] = np.cumsum(num_events) - 1
-    #shift = np.cumsum(num_events) - 1
-
-    tmp = shift[uniq_b]
-    assert(np.all(b[tmp] == uniq_b))
-    assert(np.all(b[tmp[:-1]+1] > uniq_b[:-1]))
-    assert(tmp[-1] + 1 == b.size)
-
-    start_ts = start[b]
-    stop_ts = t[shift[b]]
-
-    # normalize timestamps and polarities
-    t = (t - start_ts) / np.maximum((stop_ts - start_ts), 1e-9)
-    p = (1 - p) // 2 # (-1, 1) -> (1, 0)
+    assert len(imsize) == 2
 
     shape = tuple([bs, 4] + list(imsize))
-    idx = np.ravel_multi_index([b, p, y, x], shape)
-    shift = np.prod([2] + list(imsize))
+    res = torch.zeros(shape, dtype=dtype, device=device)
+    if events.numel() == 0:
+        return res
 
-    res = np.zeros(shape, dtype=np.float32).ravel()
-    np.add.at(res, idx, np.ones(idx.size))
-    np.maximum.at(res, idx + shift, t)
-    return torch.Tensor(res.reshape(shape))
+    x = events[:, 0].long()
+    y = events[:, 1].long()
+    t = events[:, 2]
+    p = events[:, 3].long()
+    b = events[:, 4].long()
+
+    assert (torch.abs(p) == 1).all(), f'{torch.unique(p)}'
+
+    # index of the last event for each timestamp
+    uniq_b, num_events = torch.unique(b, return_counts=True)
+    shift = torch.zeros(bs, dtype=torch.long, device=device)
+    shift[uniq_b] = torch.cumsum(num_events, dim=0) - 1
+
+    tmp = shift[uniq_b]
+    assert (b[tmp] == uniq_b).all()
+    assert (b[tmp[:-1]+1] > uniq_b[:-1]).all()
+    assert tmp[-1] + 1 == b.numel()
+
+    dt = torch.zeros(bs, dtype=dtype, device=device)
+    dt[uniq_b] = t[shift[uniq_b]] - start[uniq_b]
+    dt.clamp_(min=1e-9)
+
+    # normalize timestamps and polarities
+    t = (t - start[b]) / dt[b]
+    p = (1 - p) // 2 # (-1, 1) -> (1, 0)
+
+    idx = ((b * shape[1] + p) * shape[2] + y) * shape[3] + x
+
+    res = res.view(-1)
+    scatter_sum(torch.ones(idx.numel(), device=device, dtype=dtype), idx, out=res)
+    scatter_max(t, idx + 2 * imsize[0] * imsize[1], out=res)
+    return res.view(*shape)
 
 class Model(nn.Module):
     def __init__(self, device):
@@ -166,7 +175,12 @@ class Model(nn.Module):
         # compute event_image
         if raw:
             extended_size = self._extend_size(imsize)
-            xb = compute_event_image(events, start, stop, extended_size).to(self.device)
+            xb = compute_event_image(events,
+                                     start,
+                                     stop,
+                                     extended_size,
+                                     device=self.device,
+                                     dtype=torch.float32)
         else:
             xb = events
 
